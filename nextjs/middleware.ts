bbd3dd81c25c4b8fff0e1db1da7c86428952e3f3
James Rodriguez <jamesrodriguezpac@gmail.com>
2024-11-26 15:30:46 -0600

    fixed the .next files

--- nextjs/middleware.ts
+++ nextjs/middleware.ts
@@ -3,6 +3,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/", "/pricing", "/api/upload"]);
 
 export default clerkMiddleware((auth, request) => {
+  // if a user is not authenticated and they are trying to
+  // access a private route, redirect them to the clerk login page
   if (!auth().userId && !isPublicRoute(request)) {
     auth().protect();
   }
@@ -10,8 +12,9 @@ export default clerkMiddleware((auth, request) => {
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest|json|xml|txt|webp)).*)",
-    "/",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
