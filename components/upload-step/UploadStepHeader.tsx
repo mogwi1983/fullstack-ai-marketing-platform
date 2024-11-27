ce28322f8965ff35de5b784dc41be6c66a82a998
James Rodriguez <jamesrodriguezpac@gmail.com>
2024-11-27 09:15:52 -0600

    updated the eslint, package.json, and dependencies for Nextjs15

--- components/upload-step/UploadStepHeader.tsx
+++ components/upload-step/UploadStepHeader.tsx
@@ -1,80 +1,39 @@
 "use client";
 
 import { Upload } from "lucide-react";
-import React, { useRef, useState } from "react";
+import React from "react";
 import { Button } from "../ui/button";
-import { upload } from "@vercel/blob/client";
-import toast from "react-hot-toast";
 
 interface UploadStepHeaderProps {
-  projectId: string;
+  setBrowserFiles: React.Dispatch<React.SetStateAction<File[]>>;
+  browserFiles: File[];
+  inputFileRef: React.RefObject<HTMLInputElement>;
+  handleUpload: () => Promise<void>;
+  uploading: boolean;
 }
 
-function UploadStepHeader({ projectId }: UploadStepHeaderProps) {
-  const [browerFiles, setBrowserFiles] = useState<File[]>([]);
-  const [uploading, setUploading] = useState(false);
-
-  const inputFileRef = useRef<HTMLInputElement>(null);
-
-  // TODO: Upload Files, handle files select, handle file drop
+function UploadStepHeader({
+  setBrowserFiles,
+  browserFiles,
+  inputFileRef,
+  handleUpload,
+  uploading,
+}: UploadStepHeaderProps) {
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     if (e.dataTransfer.files) {
       setBrowserFiles(Array.from(e.dataTransfer.files));
     }
   };
+
   const handleFileSelectClick = () => {
     if (inputFileRef.current) {
       inputFileRef.current.click();
     }
   };
+
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setBrowserFiles(Array.from(e.target.files));
-    }
-  };
-
-  const getFileType = (file: File) => {
-    if (file.type.startsWith("video/")) return "video";
-    if (file.type.startsWith("audio/")) return "audio";
-    if (file.type === "text/plain") return "text";
-    if (file.type === "text/markdown") return "markdown";
-    return "other";
-  };
-  const handleUpload = async () => {
-    setUploading(true);
-    try {
-      const uploadPromises = browerFiles.map(async (file) => {
-        const fileData = {
-          projectId,
-          title: file.name,
-          fileType: getFileType(file),
-          mimeType: file.type,
-          size: file.size,
-        };
-        const filename = `${projectId}/${file.name}`;
-        const result = await upload(filename, file, {
-          access: "public",
-          handleUploadUrl: "/api/upload",
-          multipart: true,
-          clientPayload: JSON.stringify(fileData),
-        });
-
-        return result;
-      });
-
-      const uploadResults = await Promise.all(uploadPromises);
-
-      toast.success(`Successfully uploaded ${uploadResults.length} files`);
-      setBrowserFiles([]);
-      if (inputFileRef.current) {
-        inputFileRef.current.value = "";
-      }
-      //TODO: fetchFiles
-    } catch (error) {
-      console.error("Error in upload process:", error);
-      toast.error("Failed to upload one or more files.Please try again.");
-    } finally {
-      setUploading(false);
     }
   };
 
@@ -83,16 +42,16 @@ function UploadStepHeader({ projectId }: UploadStepHeaderProps) {
       <h2 className="text-xl md:text-2xl lg:text-2xl font-bold mb-8">
         Step 1: Upload Media
       </h2>
-      {/* TODO: Upload Box */}
+      {/* TODO: Upload box */}
       <div
         className="p-10 border-2 border-dashed border-main bg-white rounded-3xl text-center cursor-pointer mb-10"
         onDrop={handleDrop}
         onClick={handleFileSelectClick}
         onDragOver={(e) => e.preventDefault()}
       >
-        {browerFiles.length === 0 ? (
+        {browserFiles.length === 0 ? (
           <div>
-            <Upload className="mx-auto h-8 w-8 sm:h-10 sm:w-10 text-main " />
+            <Upload className="mx-auto h-8 w-8 sm:h-10 sm:w-10 text-main" />
             <input
               type="file"
               accept=".mp4,.txt,.md,video/*,audio/*,text/plain,text/markdown"
@@ -109,7 +68,7 @@ function UploadStepHeader({ projectId }: UploadStepHeaderProps) {
           <div>
             <h3 className="font-bold mb-2">Selected Files:</h3>
             <ul className="text-sm">
-              {browerFiles.map((file, index) => (
+              {browserFiles.map((file, index) => (
                 <li key={index}>{file.name}</li>
               ))}
             </ul>
